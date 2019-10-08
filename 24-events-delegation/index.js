1e6fcb8ebad9a7ba276549c71768dd698868a22e
Eric Kim <hyeokjungkim@gmail.com>
2019-10-08 15:37:25 -0400

    adds lecture

--- 24-events-delegation/index.js
+++ 24-events-delegation/index.js
@@ -1,8 +1,47 @@
 let tacoButton = document.getElementById("taco")
 let listOfEmojisUL = document.querySelector("#blobs")
 let newEmojiForm = document.querySelector("#emoji-form")
+// let allEmojis = listOfEmojisUL.querySelectorAll("li")
 
 // element.addEventListener("event_type", function(){})
+
+// function removeEmoji(evt){
+//  evt.target.remove()
+// }
+
+// console.log(allEmojis);
+// allEmojis.forEach(function(emojiLi){
+//
+//   emojiLi.addEventListener("click", removeEmoji)
+//
+// })
+
+// 1) Find a stable parent on the HTML //-> (querySelector, getElementById)
+// 2) Add event listener //-> (addEventListener)
+// 3) If one of the children is clicked //-> (evt.target)
+// 4) Do the event //-> (evt.target)
+
+listOfEmojisUL.addEventListener("click", function(evt){
+  if (evt.target.tagName === "LI") {
+    evt.target.remove()
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // CLICK EVENT BELOW
 
@@ -12,6 +51,8 @@ tacoButton.addEventListener("click", function(evt){
   let tacoLi = document.createElement("li")
   tacoLi.innerText = "🌮"
   listOfEmojisUL.append(tacoLi)
+
+  // tacoLi.addEventListener("click", removeEmoji )
 })
 
 // SUBMIT EVENT BELOW
@@ -27,6 +68,8 @@ function handleForm(evt){
  let newEmojiLi = document.createElement("li")
  newEmojiLi.innerText = newEmoji
  listOfEmojisUL.append(newEmojiLi)
+
+  // newEmojiLi.addEventListener("click", removeEmoji)
 }
 
 
